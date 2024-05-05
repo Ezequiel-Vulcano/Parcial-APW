d6293e092173714fb32a27ca818a14e872f98998
Ezequiel Vulcano <110461102+vulky777@users.noreply.github.com>
2024-05-05 18:36:54 -0300

    feat: termine el componente 3 de detalle

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            showBienvenida: false, //Aca cambie a false la bienvenida para que me muestre primero las preguntas = valor original (false)
+            showBienvenida: true, //Aca cambie a false la bienvenida para que me muestre primero las preguntas = valor original (false)
             showCuestionario: false, //Aca tambien cambie = valor original (true)
-            showFinal: true,
+            showFinal: false,
             contador: 1,
-            datosseleccionados: {id: "1", funcion: "sanador"},  
+            datosseleccionados: {},  
             error: "No selecciono ninguna opcion, seleccione alguna para poder continuar.",
             errorbolean: false,
-            resultadoFinal: [],
+            final: {id: "3", funcion: "ofensivo"},
             preguntas: [
                 {
                     pregunta: "¿Con cuál de las siguientes facciones te identificas mejor?",
@@ -57,17 +57,82 @@ const app = Vue.createApp({
                     nombre: "druida",
                     rol: "Tanque, Sanador, Daño",
                     descripcion: "Los druidas poseen una gran variedad de estilos de combate. Pueden llevar a cabo todos los roles: sanación, tanqueo, daño cuerpo a cuerpo y daño a distancia. Es vital que los druidas adopten la forma adecuada para cada situación ya que cada forma conlleva un propósito diferente.",
-                    img: "./img/logo/druida.avif",
+                    img: "../img/logo/druida.avif",
                     alt: "imagen de un druida"
                 },
                 {
                     id: "2",
                     funcion: "sanador",
+                    nombre: "Chaman",
+                    rol: "Tanque, Sanador, Daño",
+                    descripcion: "Durante el combate, el chamán coloca totems de control y daño en el suelo para maximizar su efectividad y ponerle las cosas más difíciles a los enemigos Los chamanes son lo suficientemente versátiles para luchar contra los enemigos de cerca o a distancia, pero los chamanes sabios eligen su camino de ataque basado en los puntos fuertes y débiles de sus enemigos.",
+                    img: "../img/logo/chaman.avif",
+                    alt: "imagen de un chaman"
+                },
+                {
+                    id: "3",
+                    funcion: "sanador",
+                    nombre: "Sacerdote",
+                    rol: "Sanador, Daño",
+                    descripcion: "Los sacerdotes usan poderosa magia de sanación para asegurarse de que tanto ellos como sus compañeros no son derribados. También controlan poderosos hechizos ofensivos a distancia, pero pueden verse sobrepasados por los enemigos debido a su fragilidad física y a su ligera armadura. Los sacerdotes más experimentados combinan el uso de sus hechizos ofensivos y de control con la importancia de mantener vivo al resto del grupo.",
+                    img: "../img/logo/sacerdote.avif",
+                    alt: "imagen de un sacerdote"
+                },
+                {
+                    id: "1",
+                    funcion: "tanque",
+                    nombre: "Guerrero",
+                    rol: "Tanque, Daño",
+                    descripcion: "Los guerreros se equipan con cuidado para el combate y se enfrentan a sus enemigos de frente, dejando que los ataques resbalen contra su pesada armadura. Usan diversas tácticas de combate y una gran variedad de tipos de armas para proteger a los combatientes menos hábiles. Los guerreros deben controlar cuidadosamente su ira (el poder detrás de sus ataques más fuertes) para maximizar su efectividad en el combate.",
+                    img: "../img/logo/guerrero.avif",
+                    alt: "imagen de un guerrero"
+                    
+                    
+                },
+                {
+                    id: "2",
+                    funcion: "tanque",
+                    nombre: "Caballero de la Muerte",
+                    rol: "Tanque, Daño",
+                    descripcion: "Los caballeros de la Muerte se enfrentan a sus enemigos de cerca, asestando golpes con sus armas con magia oscura que vuelve a los enemigos vulnerables o les inflige daño con poder oscuro. Arrastran a los enemigos a enfrentamientos uno contra uno, obligándoles a concentrar sus ataques lejos de sus compañeros más débiles. Para evitar que sus enemigos escapen de sus garras, los caballeros de la Muerte deben ser conscientes del poder que invocan de las runas y controlar sus ataques de forma apropiada.",
+                    img: "../img/logo/caballeroMuerte.avif",
+                    alt: "imagen de un caballero de la muerte"
+                },
+                {
+                    id: "3",
+                    funcion: "tanque",
                     nombre: "Paladin",
                     rol: "Tanque, Sanador, Daño",
-                    descripcion: "asehasheashashea.",
-                    img: "./img/logo/druida.avif",
-                    alt: "imagen de un druida"
+                    descripcion: "Los paladines se colocan justo delante de sus enemigos, confiando en su pesada armadura y la sanación para poder sobrevivir a una lluvia de ataques. Ya sea con enormes escudos o con aplastantes armas a dos manos, los paladines pueden aguantar zarpas y espadas de sus compañeros más débiles, pero deben usar la magia sanadora con cuidado para asegurarse de que se mantienen en pie.",
+                    img: "../img/logo/paladin.avif",
+                    alt: "imagen de un paladin"
+                },
+                {
+                    id: "1",
+                    funcion: "ofensivo",
+                    nombre: "Cazador",
+                    rol: "Daño",
+                    descripcion: "Desde temprana edad, la llamada de la naturaleza atrae a algunos aventureros desde la comodidad de sus hogares hacia el implacable mundo primario. Aquellos que aguantan se convierten en cazadores. Maestros de su entorno, son capaces de escabullirse como fantasmas entre los árboles y poner trampas en el camino de sus enemigos.",
+                    img: "../img/logo/cazador.webp",
+                    alt: "imagen de un cazador"
+                },
+                {
+                    id: "2",
+                    funcion: "ofensivo",
+                    nombre: "Mago",
+                    rol: "Daño",
+                    descripcion: "Los magos destruyen a sus enemigos con encantamientos arcanos. Aunque controlan poderosos hechizos ofensivos, los magos son frágiles y su armadura es ligera, lo que los hace particularmente vulnerables a los ataques a corta distancia. Los magos sabios usan sus hechizos con cuidado para mantener a sus enemigos a distancia o retenerlos en el lugar.",
+                    img: "../img/logo/mago.avif",
+                    alt: "imagen de un mago"
+                },
+                {
+                    id: "3",
+                    funcion: "ofensivo",
+                    nombre: "Picaro",
+                    rol: "Daño",
+                    descripcion: "Los pícaros a menudo inician sus batallas entre las sombras, comenzando con sanguinarios golpes cuerpo a cuerpo. En batallas largas, usan ataques sucesivos, cuidadosamente seleccionados para preparar al enemigo para el golpe final. Los pícaros deben tener especial cuidado al seleccionar a sus objetivos para no malgastar sus ataques de combo y deben saber cuándo esconderse o huir si la batalla se vuelve contra ellos.",
+                    img: "../img/logo/picaro.avif",
+                    alt: "imagen de un picaro"
                 }
             ]
         }
@@ -83,9 +148,9 @@ const app = Vue.createApp({
             this.showBienvenida = false;
             this.showCuestionario = false;
             this.showFinal = true
-            this.resultadoFinal.id = this.datosseleccionados[2]
-            this.resultadoFinal.funcion = this.datosseleccionados[1]
-            console.log(this.resultadoFinal)
+            this.final.id = this.datosseleccionados[2]
+            this.final.funcion = this.datosseleccionados[1]
+            console.log(this.final)
         },
 
         incrementar(){
@@ -313,12 +378,30 @@ app.component(`componente-cuestionario`, {
 })
 
 app.component(`componente-final`, {
-    props: ['clases', 'datosseleccionados'], 
+    props: ['clases', 'final'], 
 
     template: `
-        <div class="fondo-final"> 
+        <div class="fondo-final col-12"> 
             <template v-for="(item, index) in clases" :key="index">
-                <p v-if=((item.id) == )> {{item}}</p>
+                <template v-if="(((item.id) == (final.id)) && ((item.funcion) == (final.funcion)))"> 
+                    <div class=" row mt-5 justify-content-center flex-column align-items-center">
+
+                        <div class="col-12 d-flex flex-column align-items-center mb-5">
+                            <span class="tarjeta-titulo">Recomendación  de la casa</span>
+                            <p class="tarjeta-parrafo">En base a tus elecciones previamente seleccionadas, la clase que te recomendamos jugar es la siguiente.</p>
+                        </div>
+                        
+                        <div class="col-6 tarjeta-final d-flex p-0">
+                            <img :src="item.img" :alt="item.alt" class="tarjeta-imagen p-0"> 
+                            <div> 
+                                <h3>{{item.nombre}}</h3>
+                                <span>{{item.rol}}</span>
+                                <p>{{item.descripcion}}</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </template>
             </template>
         </div>
     `,
